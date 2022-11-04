2afe2e6adc32a6e4b6d6d2691b998ba20aa4a682
mkrej <mkrej>
2022-11-04 11:45:10 +0100

    w combTest opisy i próby wyjaśnienia

--- combTest.R
+++ combTest.R
@@ -1,3 +1,10 @@
+#
+# skąd ja to wziąłem ?????/!!!!!
+# pierwsza wersja chyba z 2019.01.14
+#
+# nie napisałem sobie jak to działa jak to skleciłem
+# komentarze dopisuję po fakcie próbując to zrozumieć
+#
 rm(list = ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 print(getwd())
@@ -10,51 +17,59 @@ source('doublePSD.R')
 fsample = 1000
 fullTm = 2
 
+# liczba próbek
+nsamp = fullTm * fsample
+
 carrFreq = 210
 fr = 20
-band = 100
-# band = 40
 
+# współczynnik wypełnienia
+duty = 0.1
+duty = 0.2
 
+# w funkcji sinx/x przy generowaniu modSig, w argumencie sinusa jest 1/duty 
+# czyli to okres sinusa czyli jedno z pierwszych zer sinx/x
+# więc zgrubnie ta liczba razy odstęp prążków to pasmo
+# band = fr/duty
+# w każdym razie współczynnik wypełnienia jest powiązany z pasmem
+
+# liczba prążków
+nbar = 1 / duty
+nbar = 20
+
+# patrz pulse-gen.R
+# prostsza wersja pulse train, symetryczna, bi = 0, 
+# wypełnienie jest 2x większe, bo są 2 połówki
+# zobacz w google "fourier series for common signals"
+# w https://en.wikipedia.org/wiki/Fourier_series#Table_of_common_Fourier_series
+# jest tylko ta bardziej skomplikowana
+modSig = 0
+for(i in 0:nbar)
+{
+    if (i == 0) 
+        ai = duty 
+    else 
+        ai = 1/i/pi * sin(2*pi * i * duty) 
+    carg = 2*pi * 1:(fullTm * fsample) * (fr * i) / fsample
+    modSig = modSig + 2 * ai * cos(carg)
+}
+    
 if(F)
 {
-    pd = fsample * (1 / fr  - 1 / band)
-    
-    stopifnot(pd > 0)
-    
-    modSig = 0
-    for(i in 1:(fullTm * fr))
-        modSig = c(modSig, rep(0, pd), rep(1, fsample / band))
-    modSig = modSig[1:(fullTm * fsample)]
-
-}else
-{
-    modSig = 0
-    for(i in -250:250)
-    {
-        if (i == 0) 
-            modSig = modSig + fr / band
-        else
-            modSig = modSig + 1 / i / pi * sin(pi * fr / band * i) * cos(2 * pi * 1:(fullTm * fsample) * (fr * i) / fsample)
-    }
-    
-    if(F)
-    {
-        N = length(modSig)
-        ft = fft(modSig)
-        ft = c(ft[(N / 2 + 1):N],  ft[1:(N / 2)])
-        plot(Re(ft), type = 'l', col = 'blue')
-        plot(Im(ft), type = 'l', col = 'red')
-    }
-    
-    #plot(modSig[1:(fsample * 4 / fr)], type='l')
-    
-    combSig = modSig * cos(2 * pi * 1:(fullTm * fsample) * carrFreq / fsample)
-
+    ft = fft(modSig)
+    ft = c(ft[(nsamp / 2 + 1):nsamp],  ft[1:(nsamp / 2)])
+    plot(Re(ft), type = 'l', col = 'blue')
+    plot(Im(ft), type = 'l', col = 'red')
 }
 
-plot(modSig[1:(fsample * 4 / fr)], type='l')
+#plot(modSig[1:(fsample * 4 / fr)], type='l')
 
+#combSig = modSig * cos(2 * pi * 1:(fullTm * fsample) * carrFreq / fsample)
+
+
+
+
+plot(modSig[1:(fsample * 4 / fr)], type='l')
 plot(modSig[1:(fsample * 1 / fr)], type='o')
 
 if(F)
@@ -64,8 +79,9 @@ if(F)
     plot(ps$frequency, ps$power, type = 'l')
 }
 
-
-combSig = modSig * cos(2 * pi * 1:(fullTm * fsample) * carrFreq / fsample)
+# modulacja --> przeniesienie na wyższą częstotliwość
+# czyli obwiednia to sygnał impulsu, im mniejsze pasmo tym bardziej zaokrąglony
+combSig = modSig * cos(2 * pi * 1:nsamp * carrFreq / fsample)
 
 plot(combSig[1:(fsample * 4 / fr)], type='l')
 plot(combSig, type='l')
@@ -75,7 +91,7 @@ plot(combSig, type='l')
 ps = welchPSD(ts((combSig), frequency = fsample), seglength = 1024 / fsample)
 plot(ps$frequency, ps$power, type = 'l')
 
-rn = which(ps$frequency >carrFreq - band * 1.1 & ps$frequency < carrFreq + band * 1.1)
+rn = which(ps$frequency >carrFreq - fr/duty * 1.1 & ps$frequency < carrFreq + fr/duty * 1.1)
 plot(ps$frequency[rn], ps$power[rn], type = 'l')
 
 
