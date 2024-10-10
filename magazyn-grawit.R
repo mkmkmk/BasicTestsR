48c9a9e829c9e3fea411e58f3378c00a6133f6bc
mkrej <mkrej>
2024-10-10 08:54:42 +0200

    ad grawitacyjny magazyn energii

---
+++ magazyn-grawit.R
@@ -0,0 +1,26 @@
+#' ---
+#' title: grawitacyjny magazyn energii
+#' ---
+#+ echo=F, include=F, warnings=F
+#-------------------
+
+#' ### Ad grawitacyjny magazyn energii 
+#' 
+#' 
+
+#' masa [kg] :
+m = 10000  # kg
+
+#' wysokość [m] :
+h = 10     # m
+
+#' przyspieszenie ziemskie [m/s^2] :
+g = 9.81   # m/s^2
+
+#' energia [J] :
+#' 
+h*g*m  # J
+
+#' energia [kWh] :
+h*g*m / 3.6e6  # kWh
+
